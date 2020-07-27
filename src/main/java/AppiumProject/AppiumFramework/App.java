035f0246ff9c58049e66fab1f4cccaa387274c3c
akhil-prasad94 <42665938+akhil-prasad94@users.noreply.github.com>
2020-07-27 15:50:48 -0400

    Initializing framework

---
+++ src/main/java/AppiumProject/AppiumFramework/App.java
@@ -0,0 +1,13 @@
+package AppiumProject.AppiumFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
